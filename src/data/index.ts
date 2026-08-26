8a384a6b97a5b43d15e2aa6d845ef3443f268f0a
Michael Duah <bishop10.md@gmail.com>
2026-08-26 11:42:58 -0500

    Add src/data/index.ts for v0.2 MLS Edition

---
+++ src/data/index.ts
@@ -0,0 +1,10 @@
+export * from "./types";
+export { organisms } from "./organisms";
+export { antibiotics } from "./antibiotics";
+export { mechanisms } from "./mechanisms";
+export { genes } from "./genes";
+export { breakpoints } from "./breakpoints";
+export { intrinsicPatterns } from "./intrinsicPatterns";
+export { cases } from "./cases";
+export { references } from "./references";
+
